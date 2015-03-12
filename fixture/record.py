c813e609df878c7203ed7a71753c0fcdeaf00271
Spasley <proff.spasley@gmail.com>
2015-03-12 20:37:05 +0600

    Выделен класс для работы с записями

---
+++ fixture/record.py
@@ -0,0 +1,74 @@
+__author__ = 'Spasley'
+
+class RecordHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def open_records_page(self):
+        # open records page
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+
+    def filling_form(self, Record_fields):
+        # filling form
+        wd = self.app.wd
+        self.open_records_page()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(Record_fields.fistname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(Record_fields.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(Record_fields.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(Record_fields.nickname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(Record_fields.title)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(Record_fields.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(Record_fields.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(Record_fields.home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(Record_fields.mobile)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(Record_fields.work)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(Record_fields.fax)
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(Record_fields.homepage)
+        wd.find_element_by_xpath(Record_fields.byear_day).click()
+        wd.find_element_by_xpath(Record_fields.byear_month).click()
+        wd.find_element_by_xpath(Record_fields.ayear_day).click()
+        wd.find_element_by_xpath(Record_fields.ayear_month).click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(Record_fields.byear)
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(Record_fields.ayear)
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(Record_fields.address2)
+        wd.find_element_by_name("phone2").click()
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(Record_fields.phone2)
+        wd.find_element_by_name("notes").click()
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(Record_fields.notes)
+        #wd.find_element_by_name("title").click() похоже, случайно кликнул, хз зачем это тут
+        wd.find_element_by_name("photo").click()
+        wd.find_element_by_xpath(Record_fields.photo).click()
