bd2286d4b438f146bcbb3843560d17772ce23554
Spasley <proff.spasley@gmail.com>
2015-04-15 22:27:27 +0600

    fixed Задание 14

--- test/test_record_contacts.py
+++ test/test_record_contacts.py
@@ -12,7 +12,7 @@ def test_contact_info_on_home_page(app):
     records_from_home_page = app.record.get_record_list()[index]
     records_from_edit_page = app.record.get_record_info_from_edit_page(index)
     assert records_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(records_from_edit_page)
-    assert records_from_home_page.address == merge_address_like_on_home_page(records_from_edit_page)
+    assert records_from_home_page.address == records_from_edit_page.address
     assert records_from_home_page.emails == merge_emails_like_on_home_page(records_from_edit_page)
     assert records_from_home_page.firstname == records_from_edit_page.firstname
     assert records_from_home_page.lastname == records_from_edit_page.lastname
@@ -36,6 +36,6 @@ def merge_phones_like_on_home_page(records_from_edit_page):
                                         records_from_edit_page.phone2]))))
 
 
-def merge_address_like_on_home_page(records_from_edit_page):
-    return "\n".join(filter(lambda x: x != "", filter(lambda x: x is not None, records_from_edit_page.address.split())))
+'''def merge_address_like_on_home_page(records_from_edit_page):
+    return "\n".join(filter(lambda x: x != "", filter(lambda x: x is not None, records_from_edit_page.address.split())))'''
 
