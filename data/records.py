0fc68bf8437c26a3af19e4ab5598ed5dddad21de
Spasley <proff.spasley@gmail.com>
2015-04-17 20:26:25 +0600

    + генератор и загрузка для контактов

---
+++ data/records.py
@@ -0,0 +1,9 @@
+__author__ = 'Volodya'
+from model.recordfields import RecordFields
+
+testdata = [
+    RecordFields(firstname='firstname1', middlename='middlename1', lastname='lastname1', nickname='nickname1',
+                 address='address1', company='company1'),
+    RecordFields(firstname='firstname2', middlename='middlename2', lastname='lastname2', nickname='nickname2',
+                 address='address2', company='company2'),
+]
--- test/test_record_add.py
+++ test/test_record_add.py
@@ -1,28 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.recordfields import RecordFields
-import pytest
-import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-testdata = [
-    RecordFields(firstname=firstname, middlename=middlename, lastname=lastname, nickname=nickname,
-                 address=address, company=company)
-    for firstname in ["", random_string("firstnamename", 15)]
-    for middlename in ["", random_string("middlename", 15)]
-    for lastname in ["", random_string("lastname", 15)]
-    for nickname in ["", random_string("nickname", 15)]
-    for address in ["", random_string("address", 15)]
-    for company in ["", random_string("company", 15)]
-]
-
-
-@pytest.mark.parametrize("record", testdata, ids=[repr(x) for x in testdata])
-def test_test_record_add(app, record):
+def test_test_record_add(app, json_records):
+    record = json_records
     old_records = app.record.get_record_list()
     app.record.filling_form(record)
     assert len(old_records) + 1 == app.record.count()
