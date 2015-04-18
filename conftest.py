7f22b709ab1b4df636be5b8c692aaec2e149d982
Spasley <proff.spasley@gmail.com>
2015-04-18 10:41:51 +0600

    проверка внесенных изменений в дб

--- conftest.py
+++ conftest.py
@@ -41,6 +41,8 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--check_ui", action="store_true")
+
 
 
 def pytest_generate_tests(metafunc):
@@ -70,3 +72,8 @@ def db(request):
         dbfixture.destroy()
     request.addfinalizer(fin)
     return dbfixture
+
+
+@pytest.fixture
+def check_ui(request):
+    return request.config.getoption("--check_ui")
