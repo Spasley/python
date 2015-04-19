7cb4c2170c516fc0ce6e7827c0ba88142019a678
Spasley <proff.spasley@gmail.com>
2015-04-19 10:51:35 +0600

    проверка по бд и опция для добавления контакта

--- test/test_record_add.py
+++ test/test_record_add.py
@@ -2,14 +2,15 @@
 from model.recordfields import RecordFields
 
 
-def test_test_record_add(app, json_records):
+def test_test_record_add(app, json_records, check_ui, db):
     record = json_records
-    old_records = app.record.get_record_list()
+    old_records = db.get_record_list()
     app.record.filling_form(record)
-    assert len(old_records) + 1 == app.record.count()
-    new_records = app.record.get_record_list()
+    new_records = db.get_record_list()
     old_records.append(record)
     assert sorted(old_records, key=RecordFields.id_or_max) == sorted(new_records, key=RecordFields.id_or_max)
+    if check_ui:
+        assert (sorted(app.contact.get_contact_list(), key=RecordFields.id_or_max) == sorted(new_records, key=RecordFields.id_or_max))
 
 
 
