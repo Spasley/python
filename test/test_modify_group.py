2ea1a8d290f710b71024d51c36d16416f53c8ef8
Spasley <proff.spasley@gmail.com>
2015-04-19 10:28:30 +0600

    +check_ui для добавления группы

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,27 +1,17 @@
 __author__ = 'Spasley'
 from model.group import Group
-from random import randrange
+import random
 
 
-def test_modify_group_name(app):
-    if app.group.count() == 0:
+def test_modify_group_name(app, db, check_ui, json_groups):
+    #new_group_data = json_group
+    new_group_data = Group(name='very_new_name')
+    if len(db.get_group_list()) == 0:
         app.group.create(Group(name='Test'))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    group = Group(name='new_name')
-    group.id = old_groups[index].id
-    app.group.modify_group_by_index(group, index)
-    assert len(old_groups) == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index] = group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
+    app.group.modify_group_by_id(new_group_data, group.id)
+    new_groups = db.get_group_list()
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
-'''def test_modify_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name='Test'))
-    app.group.modify_first_group(Group(header='new_header'))
-
-def test_modify_group_footer(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name='Test'))
-    app.group.modify_first_group(Group(footer='new_footer'))'''
